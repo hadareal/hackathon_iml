79aa815da898dcb7c22e70416d406a083229a74c
hrappeport <hrappeport@gmail.com>
2018-06-28 02:49:35 +0300

    Better version with many many important changes. Wait and see

--- Clusterer.py
+++ Clusterer.py
@@ -1,2 +1,48 @@
+import parser
+from random import choice
+import numpy as np
+import matplotlib.pyplot as plt
 
 
+ITERS = 10
+
+def num_of_occurences(string, substring):
+    return string.count(substring)
+
+def excpected_num_of_occurences(string_len, substring):
+    s = 0
+    for i in range(ITERS):
+        string = ''.join(choice(['0', '1']) for i in xrange(string_len))
+        cur = num_of_occurences(string, substring)
+        s += cur
+    return s/ITERS
+
+def stats(data, str):
+    r= []
+    for i in  range(len(data)):
+        n = float(num_of_occurences(data[i], str))
+        m = float(excpected_num_of_occurences(len(data[i]), str))
+        if n==0: n+=1
+        r.append(m/n)
+    plt.plot(np.arange(iters+1),losses, 'r-')
+    plt.title('Gradient descent 0-1 loss')
+    plt.ylabel('0-1 loss')
+    plt.xlabel('Iteration')
+
+    # plt.scatter()
+    plt.show()
+
+    plt.scatter
+
+
+
+def main():
+
+
+    data = parser.get_data_from_file()
+    stats(data, "001")
+
+
+
+if __name__ == "__main__":
+    main()
