0fdc5928eb984e55961f0fc83fb3d77b83cda073
hrappeport <hrappeport@gmail.com>
2018-06-28 04:18:19 +0300

    NO VAULT

--- Clusterer.py
+++ Clusterer.py
@@ -6,8 +6,14 @@ import math
 
 ITERS = 10
 
+
+def bin_derivative(v):
+    return [0 if v[i] == v[i + 1] else 1 for i in range(len(v) - 1)]
+
+
 def num_of_occurences(string, substring):
     return string.count(substring)
+
 
 def excpected_num_of_occurences(string_len, substring):
     s = 0
@@ -15,49 +21,36 @@ def excpected_num_of_occurences(string_len, substring):
         string = ''.join(choice(['0', '1']) for i in xrange(string_len))
         cur = num_of_occurences(string, substring)
         s += cur
-    return s/ITERS
+    return s / ITERS
+
 
 def stats(data, str):
-    r= []
-    for i in  range(len(data)):
+    r = []
+    for i in range(len(data)):
         n = float(num_of_occurences(data[i], str))
         m = float(excpected_num_of_occurences(len(data[i]), str))
-        if n==0: n+=1
-        r.append(m/n)
-    plt.plot(np.arange(iters+1),lossses, 'r-')
-    plt.title('Gradient descent 0-1 loss')
-    plt.ylabel('0-1 loss')
-    plt.xlabel('Iteration')
-
-    # plt.scatter()
-    plt.show()
-
-    plt.scatter
-
+        if n == 0: n += 1
+        r.append(m / n)
+        # plt.plot(np.arange(iters+1),lossses, 'r-')
+        # plt.title('Gradient descent 0-1 loss')
+        # plt.ylabel('0-1 loss')
+        # plt.xlabel('Iteration')
+        #
+        # # plt.scatter()
+        # plt.show()
+        #
+        # plt.scatter
 
 
 def main():
+    s = ''.join(choice(['0', '1']) for i in xrange(20))
+    print s
+    print bin_derivative(s)
 
-
-    data = parser.get_data_from_file()
-    stats(data, "001")
-
+    #
+    # data = parser.get_data_from_file()
+    # stats(data, "001")
 
 
 if __name__ == "__main__":
     main()
-
-
-def splitTrainTest(data, labels, trainPer):
-    """
-
-    :param data: mxd matrix of m feature samples
-    :param labels: list of m labels
-    :param trainPer: Percentage of data used for training
-    :return: trainData, testData, trainLabels, testLabels
-    """
-    m, d = data.shape
-    indices = np.random.permutation(m)
-    training_size = math.floor(m * (trainPer / 100))
-    training_idx, test_idx = indices[:training_size], indices[training_size:]
-    return data[training_idx, :], data[test_idx, :], labels[training_idx], labels[test_idx]
