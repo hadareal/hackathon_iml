c491031613fd33226e8f3919e8bd9abc1138973c
Hadar Eliyahu <h30816036@cs.huji.ac.il>
2018-06-28 04:28:10 +0300

    updating split_train_test method

--- classifier.py
+++ classifier.py
@@ -2,14 +2,21 @@
 
 import numpy as np
 from sklearn.svm import SVC
+from sklearn.ensemble import AdaBoostClassifier
 import matplotlib.pyplot as plt
 import parser
 from ex4_tools import DecisionStump, decision_boundaries
 import math
 
-TEST_SIZE = 150
+TRAIN_PERCENTAGE = 90.0
 
-def classifier(samples, labels):
+def svm(samples, labels):
+    """
+    Create svm classifier and train it on the samples
+    :param samples:
+    :param labels:
+    :return: svm classifier
+    """
 
     svm = SVC(kernel='sigmoid')
     svm.fit(samples,labels)
@@ -31,9 +38,17 @@ def splitTrainTest(data, labels, trainPer):
     training_idx, test_idx = indices[:training_size], indices[training_size:]
     return data[training_idx, :], data[test_idx, :], np.ravel(labels[training_idx]), np.ravel(labels[test_idx])
 
-data,labels,vault = parser.get_normalized_data()
+def adaboost(samples, labels):
+    """
 
-samples, test_points, labels, test_points_labels = splitTrainTest(data, labels, 90.0)
+    :param samples:
+    :param labels:
+    :return:
+    """
+    ada = AdaBoostClassifier()
+
+data,labels = parser.get_normalized_data()
+samples, test_points, labels, test_points_labels = parser.split_train_test(data, labels, TRAIN_PERCENTAGE)
 svm = SVC(kernel='sigmoid')
 svm.fit(samples, labels)
 print(svm.score(test_points, test_points_labels))
