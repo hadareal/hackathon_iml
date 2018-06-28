e9b2140cadc708f3ee32956cfc9e20f392dc7ef9
hrappeport <hrappeport@gmail.com>
2018-06-28 06:42:35 +0300

    FEATURES!!!!!!!!!!!!111

--- Clusterer.py
+++ Clusterer.py
@@ -1,93 +1,80 @@
 import parser
 from random import choice
 import numpy as np
-import matplotlib.pyplot as plt
-import random
-import math
-import collections
+import itertools
+
+D = parser.D
+ITERS = 100
 
 
-ITERS = 10
+class clusterer:
+    def __init__(self):
+        self.substrings = ['0', '1', '00', '01', '10','11', '000', '001', '010', '011', '100', '101', '110', '111']
+        self.expected = [self.excpected_num_of_occurences(D, ss) for ss in self.substrings]
 
+    def bin_derivative(self, v):
+        return [0 if v[i] == v[i + 1] else 1 for i in range(len(v) - 1)]
 
-def bin_derivative(v):
-    return ([0 if v[i] == v[i + 1] else 1 for i in range(len(v) - 1)])
+    def ftr(self, v):
 
-def matrix_bin_derivative(X):
-    """
+        n = len(v)
+        features = []
+        s_v = ''.join([str(c) for c in v])
 
-    :param X: mXd ndarray to be differentiated
-    :return: mX(d-1) differentiated ndarray
-    """
-    m,d = X.shape
-    R = [0]*m
-    for i in range(m):
-        R[i] = bin_derivative(X[i])
-    return np.stack(R)
+        for i, substring in enumerate(self.substrings):
+            m = float(self.num_of_occurences(s_v, substring))
+            features.append(m / self.expected[i])
+        return features
 
+    def matrix_bin_derivative(self, X):
+        """
 
+        :param X: mXd ndarray to be differentiated
+        :return: mX(d-1) differentiated ndarray
+        """
+        m, d = X.shape
+        R = [0] * m
+        for i in range(m):
+            R[i] = self.bin_derivative(X[i])
+        return np.stack(R)
 
-def num_of_occurences(string, substring):
-    return string.count(substring)
+    def matrix_feature_extracter(self, X):
+        """
+        :param X: mXd ndarray to be featured
+        :return: mX(d-1) featured ndarray
+        """
+        m, d = X.shape
+        R = [0] * m
+        for i in range(m):
+            R[i] = self.ftr(X[i])
+        return np.stack(R)
 
+    def num_of_occurences(self, string, substring):
 
-def excpected_num_of_occurences(string_len, substring):
-    s = 0
-    for i in range(ITERS):
-        string = ''.join(choice(['0', '1']) for i in xrange(string_len))
-        cur = num_of_occurences(string, substring)
-        s += cur
-    return s / ITERS
+        return string.count(substring)
 
+    def excpected_num_of_occurences(self, string_len, substring):
+        s = 0
+        for i in range(ITERS):
+            string = ''.join(choice(['0', '1']) for i in xrange(string_len))
+            cur = self.num_of_occurences(string, substring)
+            s += cur
+        return s / ITERS
 
-def stats(data, str):
-    r = []
-    for i in range(len(data)):
-        n = float(num_of_occurences(data[i], str))
-        m = float(excpected_num_of_occurences(len(data[i]), str))
-        if n == 0: n += 1
-        r.append(m / n)
-        # plt.plot(np.arange(iters+1),lossses, 'r-')
-        # plt.title('Gradient descent 0-1 loss')
-        # plt.ylabel('0-1 loss')
-        # plt.xlabel('Iteration')
-        #
-        # # plt.scatter()
-        # plt.show()
-        #
-        # plt.scatter
+    def stats(self, data, str):
+        r = []
+        for i in range(len(data)):
+            n = float(self.num_of_occurences(data[i], str))
+            m = float(self.excpected_num_of_occurences(len(data[i]), str))
+            if n == 0: n += 1
+            r.append(m / n)
 
 
 def main():
-    # s = ''.join(choice(['0', '1']) for i in xrange(20))
-    # n=100000
-    data , labels= parser.get_normalized_data()
+    data, labels = parser.get_normalized_data()
     print data.shape
-    print data.shape
-    d_data = matrix_bin_derivative(data)
-    print d_data.shape
-
-
-
-    # t = data[5]
-    # n = float(len(t))
-    # e = []
-    # for i in range(20):
-    #     e.append(t.count('0')/n)
-    #     t = bin_derivative(t)
-    #
-    # s = [str(random.randint(0,1)) for i in range(int(n))]
-    # s = ''.join(s)
-    # n = float(len(s))
-    # d = []
-    # for i in range(20):
-    #     d.append(s.count('0')/n)
-    #     s = bin_derivative(s)
-    #
-    # plt.plot(e)
-    # plt.show()
-    # #
-
+    c = clusterer()
+    d_data = c.matrix_feature_extracter(data)
 
 
 if __name__ == "__main__":
